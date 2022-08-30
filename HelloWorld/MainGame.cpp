0c0f6ce216dc13e960e739dbc15f62a09b838676
maliwian <lolita.svetlaya@gmail.com>
2022-08-30 02:37:40 +0100

    initial commit, vertical movement of the spider with the music and animation

--- HelloWorld/MainGame.cpp
+++ HelloWorld/MainGame.cpp
@@ -2,23 +2,74 @@
 #define PLAY_USING_GAMEOBJECT_MANAGER
 #include "Play.h"
 
-int DISPLAY_WIDTH = 640;
-int DISPLAY_HEIGHT = 360;
-int DISPLAY_SCALE = 2;
+int DISPLAY_WIDTH = 1280;
+int DISPLAY_HEIGHT = 720;
+int DISPLAY_SCALE = 1;
+
+struct GameState
+{
+	float timer = 0;
+	int score = 0;
+};
+
+GameState gameState;
+
+enum GameObjectType
+{
+	TYPE_NULL = -1,
+	TYPE_AGENT8,
+};
+
+void HandlePlayerControls();
 
 // The entry point for a PlayBuffer program
 void MainGameEntry( PLAY_IGNORE_COMMAND_LINE )
 {
 	Play::CreateManager( DISPLAY_WIDTH, DISPLAY_HEIGHT, DISPLAY_SCALE );
+	Play::CentreAllSpriteOrigins();
+	Play::LoadBackground( "Data\\Backgrounds\\background.png" );
+	Play::StartAudioLoop( "music" );
+	Play::CreateGameObject( TYPE_AGENT8, {115, 0}, 50, "agent8" );
 }
 
 // Called by PlayBuffer every frame (60 times a second!)
 bool MainGameUpdate( float elapsedTime )
 {
-	Play::ClearDrawingBuffer( Play::cOrange );
-	Play::DrawDebugText( { DISPLAY_WIDTH / 2, DISPLAY_HEIGHT / 2 }, "Hello World!" );
+	Play::DrawBackground();
+	HandlePlayerControls();
+
 	Play::PresentDrawingBuffer();
 	return Play::KeyDown( VK_ESCAPE );
+}
+
+void HandlePlayerControls()
+{
+	GameObject& obj_agent8 = Play::GetGameObjectByType( TYPE_AGENT8 );
+	if (Play::KeyDown( VK_UP ))
+	{
+		obj_agent8.velocity = { 0, -4 };
+		Play::SetSprite( obj_agent8, "agent8_climb", 0.25f );
+	}
+	else if (Play::KeyDown( VK_DOWN ))
+	{
+		obj_agent8.acceleration = { 0, 1 };
+		Play::SetSprite( obj_agent8, "agent8_fall", 0 );
+	}
+	else
+	{
+		Play::SetSprite( obj_agent8, "agent8_hang", 0.02f );
+		obj_agent8.velocity *= 0.5f;
+		obj_agent8.acceleration = { 0, 0 };
+	}
+	Play::UpdateGameObject( obj_agent8 );
+
+	if (Play::IsLeavingDisplayArea( obj_agent8 ))
+	{
+		obj_agent8.pos = obj_agent8.oldPos;
+	}
+
+	Play::DrawLine( { obj_agent8.pos.x, 0 }, obj_agent8.pos, Play::cWhite );
+	Play::DrawObjectRotated( obj_agent8 );
 }
 
 // Gets called once when the player quits the game 
